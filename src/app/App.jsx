45b616d986a1bc00fb74a69d1389f03dd33b66b0
Carrotist <romark99@gmail.com>
2026-07-02 16:56:29 +0300

    fix(shared): Dropdown values bug, custom arrow dropdown svg icon

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,7 @@
 import '@shared/ui/styles';
 import { Button } from '@shared/ui/Button';
 import { Input } from '@shared/ui/Input';
+import { Dropdown } from '@shared/ui/Dropdown';
 
 export const App = () => {
   return (
@@ -8,6 +9,7 @@ export const App = () => {
       <Button>Sign contract</Button>
       <Button type="secondary">Back</Button>
       <Input />
+      <Dropdown options={['Test1', 'Test2', 'Test3']} />
     </>
   );
 };
--- src/shared/ui/Dropdown/Dropdown.jsx
+++ src/shared/ui/Dropdown/Dropdown.jsx
@@ -3,12 +3,17 @@ import styles from '@shared/ui/styles/input-base.module.css';
 export const Dropdown = ({ name, options = [], ...props }) => {
   const style = `${styles.select}`;
   return (
-    <select name={name} className={style} {...props}>
-      {options.map((option, index) => (
-        <option key={index} value={option[index]}>
-          {option[index]}
-        </option>
-      ))}
-    </select>
+    <div className={styles.wrapper}>
+      <select name={name} className={style} {...props}>
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+      <svg className={styles.icon}>
+        <use href="/assets/icons/arrow-dropdown.svg"></use>
+      </svg>
+    </div>
   );
 };
