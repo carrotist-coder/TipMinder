391211fd45504794d78a7dbf59db826f88e0adfb
Carrotist <romark99@gmail.com>
2026-07-02 11:15:44 +0300

    chore: init project with webpack, babel, eslint configs

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+const js = require('@eslint/js');
+const reactPlugin = require('eslint-plugin-react');
+const reactHooksPlugin = require('eslint-plugin-react-hooks');
+
+module.exports = [
+    js.configs.recommended,
+    reactPlugin.configs.flat.recommended,
+    {
+        files: ['**/*.{js,jsx}'],
+        plugins: {
+            'react-hooks': reactHooksPlugin,
+        },
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            parserOptions: {
+                ecmaFeatures: { jsx: true },
+            },
+            globals: {
+                document: 'readonly',
+                window: 'readonly',
+                console: 'readonly',
+            },
+        },
+        rules: {
+            ...reactHooksPlugin.configs.recommended.rules,
+            'react/react-in-jsx-scope': 'off',
+        },
+        settings: {
+            react: { version: 'detect' },
+        },
+    },
+];
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.[contenthash].js',
+        clean: true,
+        publicPath: '/',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './public/index.html' }),
+        new ESLintPlugin({ extensions: ['js', 'jsx'] }),
+    ],
+    devServer: {
+        port: 3000,
+        hot: true,
+        open: true,
+        historyApiFallback: true,
+    },
+};
