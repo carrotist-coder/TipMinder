a70f5a1c6472eb7f75981761891ba7efc85bd915
Carrotist <romark99@gmail.com>
2026-07-02 18:37:12 +0300

    refactor(shared): delete unnecessary constants

--- src/shared/ui/Dropdown/Dropdown.jsx
+++ src/shared/ui/Dropdown/Dropdown.jsx
@@ -1,10 +1,9 @@
 import styles from '@shared/ui/styles/input-base.module.css';
 
 export const Dropdown = ({ name, options = [], ...props }) => {
-  const style = `${styles.select}`;
   return (
     <div className={styles.wrapper}>
-      <select name={name} className={style} {...props}>
+      <select name={name} className={styles.select} {...props}>
         {options.map((option) => (
           <option key={option} value={option}>
             {option}
--- src/shared/ui/Input/Input.jsx
+++ src/shared/ui/Input/Input.jsx
@@ -1,6 +1,12 @@
 import styles from '@shared/ui/styles/input-base.module.css';
 
 export const Input = ({ name, onSubmit, ...props }) => {
-  const style = `${styles.input}`;
-  return <input className={style} name={name} onSubmit={onSubmit} {...props} />;
+  return (
+    <input
+      className={styles.input}
+      name={name}
+      onSubmit={onSubmit}
+      {...props}
+    />
+  );
 };
