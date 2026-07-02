99b1ab888084e136867b7ebbf9163e0b6c673612
Carrotist <romark99@gmail.com>
2026-07-02 12:26:44 +0300

    chore: Prettier config and formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,35 @@
 const js = require('@eslint/js');
 const reactPlugin = require('eslint-plugin-react');
 const reactHooksPlugin = require('eslint-plugin-react-hooks');
+const prettierConfig = require('eslint-config-prettier');
 
 module.exports = [
-    js.configs.recommended,
-    reactPlugin.configs.flat.recommended,
-    {
-        files: ['**/*.{js,jsx}'],
-        plugins: {
-            'react-hooks': reactHooksPlugin,
-        },
-        languageOptions: {
-            ecmaVersion: 'latest',
-            sourceType: 'module',
-            parserOptions: {
-                ecmaFeatures: { jsx: true },
-            },
-            globals: {
-                document: 'readonly',
-                window: 'readonly',
-                console: 'readonly',
-            },
-        },
-        rules: {
-            ...reactHooksPlugin.configs.recommended.rules,
-            'react/react-in-jsx-scope': 'off',
-        },
-        settings: {
-            react: { version: 'detect' },
-        },
+  js.configs.recommended,
+  reactPlugin.configs.flat.recommended,
+  {
+    files: ['**/*.{js,jsx}'],
+    plugins: {
+      'react-hooks': reactHooksPlugin,
     },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        document: 'readonly',
+        window: 'readonly',
+        console: 'readonly',
+      },
+    },
+    rules: {
+      ...reactHooksPlugin.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
+  prettierConfig,
 ];
--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,38 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/app/index.jsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.[contenthash].js',
-        clean: true,
-        publicPath: '/',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: 'babel-loader',
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({ template: './public/index.html' }),
-        new ESLintPlugin({ extensions: ['js', 'jsx'] }),
+  mode: 'development',
+  entry: './src/app/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
+    clean: true,
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    devServer: {
-        port: 3000,
-        hot: true,
-        open: true,
-        historyApiFallback: true,
-    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './public/index.html' }),
+    new ESLintPlugin({ extensions: ['js', 'jsx'] }),
+  ],
+  devServer: {
+    port: 3000,
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+  },
 };
