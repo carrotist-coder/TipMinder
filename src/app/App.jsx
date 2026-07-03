731118d896078daed2b5697e798e3f37d9490053
Carrotist <romark99@gmail.com>
2026-07-03 10:29:40 +0300

    feat(shared): Segmented Control, button cursor pointer

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,14 +2,23 @@ import '@shared/ui/styles';
 import { Button } from '@shared/ui/Button';
 import { Input } from '@shared/ui/Input';
 import { Dropdown } from '@shared/ui/Dropdown';
+import { SegmentedControl } from '@shared/ui/SegmentedControl';
+import { useState } from 'react';
 
 export const App = () => {
+  // Test
+  const [option, setOption] = useState(0);
   return (
     <>
       <Button>Sign contract</Button>
       <Button type="secondary">Back</Button>
       <Input />
       <Dropdown options={['Test1', 'Test2', 'Test3']} />
+      <SegmentedControl
+        options={['Test1', 'Test2', 'Test3', 'Test4']}
+        activeIndex={option}
+        onChange={(index) => setOption(index)}
+      />
     </>
   );
 };
