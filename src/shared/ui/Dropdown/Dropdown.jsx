b17d535b9153e546c064d22a16f9dc49a2ec00a3
Carrotist <romark99@gmail.com>
2026-07-06 17:01:14 +0300

    feat(shared): add label, id to Dropdown component

--- src/shared/ui/Dropdown/Dropdown.jsx
+++ src/shared/ui/Dropdown/Dropdown.jsx
@@ -1,18 +1,27 @@
 import styles from '@shared/ui/styles/input-base.module.css';
 
-export const Dropdown = ({ name, options = [], ...props }) => {
+export const Dropdown = ({
+  name,
+  label = null,
+  id = name,
+  options = [],
+  ...props
+}) => {
   return (
-    <div className={styles.wrapper}>
-      <select name={name} className={styles.select} {...props}>
-        {options.map((option) => (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
-      <svg className={styles.icon}>
-        <use href="/assets/icons/arrow-dropdown.svg"></use>
-      </svg>
-    </div>
+    <>
+      {label && <label htmlFor={id}>{label}</label>}
+      <div className={styles.wrapper}>
+        <select id={id} name={name} className={styles.select} {...props}>
+          {options.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+        <svg className={styles.icon}>
+          <use href="/assets/icons/arrow-dropdown.svg"></use>
+        </svg>
+      </div>
+    </>
   );
 };
