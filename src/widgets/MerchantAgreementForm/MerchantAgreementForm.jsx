65ba954ffb1b567c68f5f2f9148ff1e618d9f78d
Carrotist <romark99@gmail.com>
2026-07-07 11:08:42 +0300

    feat(widgets): Merchant Agreement Form with redux state

--- src/widgets/MerchantAgreementForm/MerchantAgreementForm.jsx
+++ src/widgets/MerchantAgreementForm/MerchantAgreementForm.jsx
@@ -3,46 +3,93 @@ import { Dropdown } from '@shared/ui/Dropdown';
 import { Button } from '@shared/ui/Button';
 import { Form } from '@shared/ui/Form';
 import { SignContractButton } from '@features/merchant-agreement/SignContract/ui/SignContractButton';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateField } from '@entities/merchant-agreement/model/slice';
 
 export const MerchantAgreementForm = () => {
+  const dispatch = useDispatch();
+  const formData = useSelector((state) => state.merchantAgreement);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    dispatch(updateField({ field: name, value }));
+  };
+
   return (
     <Form
       actions={
         <>
-          <SignContractButton formData={{ test: 'test' }} />
+          <SignContractButton formData={formData} />
           <Button type="secondary">Back</Button>
         </>
       }
     >
-      <Dropdown label="Company name" name="companyName" />
-      <Dropdown label="Terminal monthly fee" name="monthlyFee" />
+      <Dropdown
+        label="Company name"
+        name="companyName"
+        value={formData.companyName}
+        onChange={handleChange}
+      />
+      <Dropdown
+        label="Terminal monthly fee"
+        name="monthlyFee"
+        value={formData.monthlyFee}
+        onChange={handleChange}
+      />
       <Input
         type="number"
         label="Personal code"
         name="personalCode"
         placeholder="Personal code"
+        value={formData.personalCode}
+        onChange={handleChange}
       />
       <Input
-        type="phone"
+        type="tel"
         label="Telephone number"
         name="phoneNumber"
         placeholder="Telephone number"
+        value={formData.phoneNumber}
+        onChange={handleChange}
       />
-      <Input type="text" label="Address" name="address" placeholder="Address" />
+      <Input
+        type="text"
+        label="Address"
+        name="address"
+        placeholder="Address"
+        value={formData.address}
+        onChange={handleChange}
+      />
       <Input
         type="text"
         label="Bank account number"
         name="bankAccount"
         placeholder="Bank account number"
+        value={formData.bankAccount}
+        onChange={handleChange}
       />
-      <Input type="email" label="Email" name="email" placeholder="Email" />
+      <Input
+        type="email"
+        label="Email"
+        name="email"
+        placeholder="Email"
+        value={formData.email}
+        onChange={handleChange}
+      />
       <Input
         type="date"
         label="Date from"
         name="dateFrom"
         placeholder="Date from"
+        value={formData.dateFrom}
+        onChange={handleChange}
       />
-      <Dropdown label="Contract number" name="contractNumber" />
+      <Dropdown
+        label="Contract number"
+        name="contractNumber"
+        value={formData.contractNumber}
+        onChange={handleChange}
+      />
     </Form>
   );
 };
