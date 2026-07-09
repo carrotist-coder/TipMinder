743d03f7e0bd3b3f19b075201f8380d8e59a2304
Carrotist <romark99@gmail.com>
2026-07-09 14:13:39 +0300

    fix(features): back button submit form bug fix

--- src/features/navigation/Back/model/service.js
+++ src/features/navigation/Back/model/service.js
@@ -2,5 +2,8 @@ import { useNavigate } from 'react-router-dom';
 
 export const useBack = () => {
   const navigate = useNavigate();
-  return () => navigate(-1);
+  return (e) => {
+    e.preventDefault();
+    navigate(-1);
+  };
 };
