1b5cc6e612ae50f4969342944448a82585abbae8
Carrotist <romark99@gmail.com>
2026-07-10 15:41:11 +0300

    feat(shared): Action Card

--- src/entities/merchant-agreement/ui/SuccessCard.js
+++ src/entities/merchant-agreement/ui/SuccessCard.js
@@ -1,19 +1,15 @@
-import styles from './SuccessCard.module.css';
-import { Heading } from '@shared/ui/Heading';
+import { ActionCard } from '@shared/ui/ActionCard';
 import { Button } from '@/shared/ui/Button';
 import { useNavigate } from 'react-router-dom';
 
 export const SuccessCard = () => {
   const navigate = useNavigate();
+
   return (
-    <div className={styles.container}>
-      <div className={styles.card}>
-        <Heading>Contract Signed!</Heading>
-        <p className={styles.text}>
-          Your contract has been successfully processed.
-        </p>
-        <Button onClick={() => navigate('/')}>Go to Dashboard</Button>
-      </div>
-    </div>
+    <ActionCard
+      heading="Contract Signed!"
+      text="Your contract has been successfully processed."
+      actions={<Button onClick={() => navigate('/')}>Go to Dashboard</Button>}
+    />
   );
 };
--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,22 +1,19 @@
-import { Heading } from '@shared/ui/Heading';
+import { ActionCard } from '@shared/ui/ActionCard';
 import { LoginButton } from '@features/auth/Login';
 import { LogoutButton } from '@features/auth/Logout';
 import { useSelector } from 'react-redux';
-import styles from './AuthPage.module.css';
 
 export const Auth = () => {
   const isAuth = useSelector((state) => state.user.isAuth);
   return (
-    <div className={styles.container}>
-      <div className={styles.card}>
-        <Heading>Profile</Heading>
-        <p className={styles.text}>
-          {isAuth
-            ? 'You are already logged in. Manage your account below.'
-            : 'Please login to access your dashboard and manage your tips.'}
-        </p>
-        {isAuth ? <LogoutButton /> : <LoginButton />}
-      </div>
-    </div>
+    <ActionCard
+      heading="Profile"
+      text={
+        isAuth
+          ? 'You are already logged in. Manage your account below.'
+          : 'Please login to access your dashboard and manage your tips.'
+      }
+      actions={isAuth ? <LogoutButton /> : <LoginButton />}
+    />
   );
 };
