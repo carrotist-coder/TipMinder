2dff127d8ac9562933b05842ffe30f0eda91d9f8
Carrotist <romark99@gmail.com>
2026-07-13 19:02:18 +0300

    refactor(constants): Auth page

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -2,16 +2,19 @@ import { ActionCard } from '@shared/ui/ActionCard';
 import { LoginButton } from '@features/auth/Login';
 import { LogoutButton } from '@features/auth/Logout';
 import { useSelector } from 'react-redux';
+import {
+  AUTH_PAGE_HEADING,
+  AUTH_PAGE_TEXT_IS_AUTH,
+  AUTH_PAGE_TEXT_IS_NOT_AUTH,
+} from '@pages/Auth/constants';
 
 export const Auth = () => {
   const isAuth = useSelector((state) => state.user.isAuth);
   return (
     <ActionCard
-      heading="Profile"
+      heading={AUTH_PAGE_HEADING}
       text={
-        isAuth
-          ? 'You are already logged in. Manage your account below.'
-          : 'Please login to access your dashboard and manage your tips.'
+        isAuth ? { AUTH_PAGE_TEXT_IS_AUTH } : { AUTH_PAGE_TEXT_IS_NOT_AUTH }
       }
       actions={isAuth ? <LogoutButton /> : <LoginButton />}
     />
