80da19f3abd4704709aaeaf5bbd2c4d59633fdb8
Carrotist <romark99@gmail.com>
2026-07-14 09:25:11 +0300

    refactor(constants): Not found 404 page, styles

--- src/pages/NotFound404/NotFound404.jsx
+++ src/pages/NotFound404/NotFound404.jsx
@@ -1,17 +1,19 @@
 import { Heading } from '@shared/ui/Heading';
 import styles from './NotFound404.module.css';
 import { GoToDashboardButton } from '@features/navigation/GoToDashboard';
+import {
+  NOT_FOUND_PAGE_ERROR_CODE,
+  NOT_FOUND_PAGE_HEADING,
+} from '@pages/NotFound404/constants';
 
 export const NotFound404 = () => {
   return (
     <section className={styles.page}>
-      <Heading>Page not found</Heading>
+      <Heading>{NOT_FOUND_PAGE_HEADING}</Heading>
       <div className={styles.container}>
-        <span className={styles.error}>404</span>
+        <span className={styles.error}>{NOT_FOUND_PAGE_ERROR_CODE}</span>
       </div>
-      <div className={styles.button}>
-        <GoToDashboardButton />
-      </div>
+      <GoToDashboardButton />
     </section>
   );
 };
